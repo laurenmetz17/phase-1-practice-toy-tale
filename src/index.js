fbad9a9c6014cb3ea6d15c6782cf3840826c050a
Lauren Metz <Laurenmetz15@gmail.com>
2022-08-22 15:56:20 -0400

    commit tree 8f2c43f6085f8202cf46e99929e8496fbafdada2

--- src/index.js
+++ src/index.js
@@ -12,4 +12,78 @@ document.addEventListener("DOMContentLoaded", () => {
       toyFormContainer.style.display = "none";
     }
   });
+
+  fetch("http://localhost:3000/toys").then(resp => resp.json()).then(toys => renderToys(toys));
+
+  let toyForm = document.querySelector('form');
+  toyForm.addEventListener('submit', createNewToy);
 });
+
+
+function renderToys(toys) {
+  toys.forEach(toy => {
+    let toyCard = document.createElement('div');
+    toyCard.classList.add('card');
+    let toyName = document.createElement('h2')
+    toyName.textContent = toy.name;
+    toyCard.append(toyName);
+    let toyImg = document.createElement('img');
+    toyImg.src = toy.image;
+    toyImg.classList.add('toy-avatar');
+    toyCard.append(toyImg);
+    let likes = document.createElement('p');
+    likes.textContent = toy.likes;
+    toyCard.append(likes);
+    let liker = document.createElement('button');
+    liker.id = toy.id;
+    liker.classList.add('like-btn');
+    liker.textContent = 'Like ❤️'
+    liker.addEventListener('click', handleLike);
+    toyCard.append(liker);
+    document.querySelector('#toy-collection').append(toyCard);
+  })
+}
+
+function createNewToy(e) {
+  e.preventDefault();
+  fetch('http://localhost:3000/toys', {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify({
+      name: document.querySelector('input[name = "name"]').value,
+      image: document.querySelector('input[name = "image"]').value,
+      likes: 0
+    })
+  }).then(resp => resp.json())
+  .then(obj => {
+    document.querySelector('#toy-collection').append(obj);
+    document.querySelector('input[name = "name"]').value = '';
+    document.querySelector('input[name = "image"]').value = '';
+  });
+}
+
+function handleLike(e) {
+  let toyId = e.target.id;
+  fetch(`http://localhost:3000/toys/${toyId}`).then(resp => resp.json())
+  .then(toy => {
+    //toyLikes not actively updating on the DOM, dont know if its becuase of the patch request or the placement of the event listener
+    console.log(document.querySelector('#toy-collection').children[toyId].querySelector('p').textContent);
+    let likeNum = toy.likes + 1;
+    fetch(`http://localhost:3000/toys/${toyId}`, {
+      method: 'PATCH',
+      headers:{
+        "Content-Type": "application/json",
+        Accept: "application/json"
+      }, 
+      body: JSON.stringify({
+        "likes": likeNum
+      })
+    }).then(resp => resp.json()).then(obj => {
+      let toyLikes = document.querySelector('#toy-collection').children[toyId].querySelector('p');
+      toyLikes.textContent = parseInt(toyLikes.textContent,10) + 1;
+    })
+  });
+}
